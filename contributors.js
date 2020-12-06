62b746e9e49d20c1f2babd335e27def5daaf87ad
Tom Chapman <chapman@live.com.au>
2020-12-06 10:01:15 +1000

    ...

--- contributors.js
+++ contributors.js
@@ -1,18 +1,15 @@
 // add your membershipId
 export default [
-    {
-        "comment" : "Horodep",
-        "language" : "russian",
-        "membershipId": "4611686018484533589"
-    },
-    {
-        "comment" : "Alse-72",
-        "language" : "italian",
-        "membershipId": "4611686018456237447"
-    },
-    {
-        "comment" : "Karl",
-        "language" : "spanish",
-        "membershipId": "4611686018428492371"
-    }
+  {
+    language: 'ru',
+    membershipId: '4611686018484533589', // Horodep
+  },
+  {
+    language: 'it',
+    membershipId: '4611686018456237447', // Alse-72
+  },
+  {
+    language: 'es',
+    membershipId: '4611686018428492371', // Karl
+  },
 ];
