1b8c30e6b5727fadb237aea261deca94db893203
Tom Chapman <chapman@live.com.au>
2020-12-12 07:30:21 +1000

    ...

--- contributors.js
+++ contributors.js
@@ -14,4 +14,19 @@ export default [
     membershipType: 2,
     membershipId: '4611686018428492371', // Karl
   },
+  {
+    language: 'de',
+    membershipType: 2,
+    membershipId: '4611686018496022776', // Parama91
+  },
+  {
+    language: 'pl',
+    membershipType: 3,
+    membershipId: '4611686018475647305', // Gutmanovaty
+  },
+  {
+    language: 'pt-br',
+    membershipType: 1,
+    membershipId: '4611686018446596986', // Dekayra
+  },
 ];
